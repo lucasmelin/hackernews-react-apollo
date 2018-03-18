e17744074bf90e81a19df6da8c3f54d243c36026
Lucas Melin <lucas.melin@gmail.com>
2018-03-18 10:17:21 -0400

    Added Search component

---
+++ src/components/Search.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { withApollo } from "react-apollo";
+import gql from "graphql-tag";
+import Link from "./Link";
+
+class Search extends Component {
+  state = {
+    links: [],
+    filter: ""
+  };
+  render() {
+    return (
+      <div>
+        <div>
+          Search
+          <input
+            typ="text"
+            onChange={e => this.setState({ filter: e.target.value })}
+          />
+          <button onClick={() => this._executeSearch()}>OK</button>
+        </div>
+        {this.state.links.map((link, index) => (
+          <Link key={link.id} link={link} index={index} />
+        ))}
+      </div>
+    );
+  }
+
+  _executeSearch = async () => {
+    // to be implemented
+  };
+}
+
+export default withApollo(Search);
