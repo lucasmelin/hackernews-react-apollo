eb810a7f51b75322659efb76bf4085ec1819062e
Lucas Melin <lucas.melin@gmail.com>
2018-03-18 21:59:39 -0400

    Added search filtering

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,8 +27,40 @@ class Search extends Component {
   }
 
   _executeSearch = async () => {
-    // to be implemented
+    const { filter } = this.state;
+    // Execute the searc hquery
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter }
+    });
+    // Get the resulting links and put them in the state
+    // for rendering
+    const links = result.data.feed.links;
+    this.setState({ links });
   };
 }
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default withApollo(Search);
